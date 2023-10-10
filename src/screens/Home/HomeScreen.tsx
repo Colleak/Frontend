d61657213e18b04a5ea40eb0fb160ccb9b4aca27
Jelle07 <113422379+JManders07@users.noreply.github.com>
2023-10-10 10:57:49 +0200

    Made Alert
    Alert for asking location permission. Alsoo added a text line where you can see if you granted permission or denied it.

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,5 +1,5 @@
-import {StyleSheet, View, Text} from 'react-native';
-import React from "react";
+import {StyleSheet, View, Text, Alert} from 'react-native';
+import React, {useState, useEffect} from "react";
 import CustomMap from "./components/CustomMap";
 import EmployeeList from "../../components/EmployeeList";
 import NavBar from "../../components/NavBar";
@@ -10,21 +10,53 @@ type HomeScreenProps = {
 
 const floor = "Floor 1";
 
+
 const HomeScreen: React.FC<HomeScreenProps> = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            <NavBar logoSource={require("../../assets/images/ioLogoBlue.png")}/>
-            <View style={styles.mapContainer}>
-                <CustomMap
-                    mapSource={require("../../assets/images/penguinmapblue.png")}
-                    markerSource={require("../../assets/images/wifi-signal-marker.png")}
-                    markerCoords={{ x: 0, y: 830 }}
-                />
-                <Text style={styles.floorText}>{floor}</Text>
-            </View>
-            <EmployeeList />
-        </View>
+  
+  const [permissionForLocation, setPermissionForLocation] = useState(false);
+  const askForLocationPermissionAlert = () => {
+    Alert.alert(
+      "Location Permission Needed",
+      "This app needs the location permission, in order to show you the map of the office.",
+      [
+        {
+          text: "Yes",
+          onPress: () => {
+            setPermissionForLocation(true);
+          },
+        },
+        {
+          text: "No",
+          onPress: () => {
+            setPermissionForLocation(false);
+          },
+        },
+      ],
+      { cancelable: false }
     );
+  };
+  useEffect(() => {
+    askForLocationPermissionAlert();
+  }, []);
+  return (
+    <View style={styles.container}>
+      <NavBar logoSource={require("../../assets/images/ioLogoBlue.png")} />
+      <Text>
+        {permissionForLocation
+          ? "Location permission granted!"
+          : "Location permission denied!"}
+      </Text>
+      <View style={styles.mapContainer}>
+        <CustomMap
+          mapSource={require("../../assets/images/penguinmapblue.png")}
+          markerSource={require("../../assets/images/wifi-signal-marker.png")}
+          markerCoords={{ x: 0, y: 830 }}
+        />
+        <Text style={styles.floorText}>{floor}</Text>
+      </View>
+      <EmployeeList />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
