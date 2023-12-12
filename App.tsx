6afabc217de39cb8f67be0e72c036e4b43205a2b
vlottetonny <57642354+vlottetonny@users.noreply.github.com>
2023-12-12 10:16:50 +0100

    alles kapot gemaakt deel 2

--- App.tsx
+++ App.tsx
@@ -7,9 +7,11 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
 import LoadingComponent from "./src/components/LoadingComponents";
 import {useAuth0, Auth0Provider} from 'react-native-auth0';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
     const [fontsLoaded, setFontsLoaded] = useState(false);
+    const { authorize, clearCredentials } = useAuth0();
 
     useEffect(() => {
         async function loadFontsAsync() {
@@ -23,10 +25,26 @@ export default function App() {
         loadFontsAsync();
     }, []);
 
+    useEffect(() => {
+        const checkUser = async () => {
+            const user = await AsyncStorage.getItem('user');
+            if (!user) {
+                authorize({ scope: 'openid profile email' })
+                .then(credentials => {
+                    // Successfully logged in
+                    // Store the user's name and id in AsyncStorage
+                    AsyncStorage.setItem('user', JSON.stringify(credentials));
+                })
+                .catch(error => console.log(error));
+            }
+        };
+
+        checkUser();
+    }, []);
+
     if (!fontsLoaded) {
         return <LoadingComponent/>;
     }
-
 
     return (
         <Auth0Provider domain={"dev-lohb1xoklmc7vqfg.us.auth0.com"} clientId={"1DEAvHt6GnbL0VApHfpYZgbuVAu0VqdC"}>
